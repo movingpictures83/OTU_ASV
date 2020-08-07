6a8bb0459be1bd342dde0dde87666d106068b0b5
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-08-07 10:22:35 -0400

    Initial

--- getPlugins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/movingpictures83/CSVNormalize.git ../../plugins/CSVNormalize
-git clone https://github.com/movingpictures83/CSVTranspose.git ../../plugins/CSVTranspose
-git clone https://github.com/movingpictures83/Bar.git ../../plugins/Bar
-
